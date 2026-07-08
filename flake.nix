d94c00e5f066d36af60cc4a685b21c1a71456a5b
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-07-08 01:42:07 -0400

    Add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, rust-overlay, ... }:
+    {
+      devShells = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" ] (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Use nightly for formatting only
+              rust-bin.nightly.latest.rustfmt
+              (rust-bin.stable."1.96.1".minimal.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                  "clippy"
+                ];
+              })
+
+              codebook
+              just
+            ];
+          };
+        }
+      );
+    };
+}
